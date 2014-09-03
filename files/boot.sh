ab545c34dddd267e4d99386e239fe5dd90dc3d6e
Tom Deckers <tom@ducbase.com>
2014-09-03 05:13:34 -0700

    Add explicit configuration file for supervisord

--- files/boot.sh
+++ files/boot.sh
@@ -5,9 +5,9 @@ ETH0_FOUND=`grep "eth0" /proc/net/dev`
 if [ -n "$ETH0_FOUND" ] ;
 then 
   # We're in a container with regular eth0 (default)
-  exec /usr/bin/supervisord
+  exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 else 
   # We're in a container without initial network.  Wait for it...
   /usr/local/bin/pipework --wait
-  exec /usr/bin/supervisord
+  exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 fi
