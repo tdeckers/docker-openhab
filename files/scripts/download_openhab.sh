0794450bc441c89fb23d5e515d58da645d4f949a
Tim Weyand <tim@weyand.biz>
2014-12-10 16:19:25 +0100

    Added openHAB Snapshot Download

--- files/scripts/download_openhab.sh
+++ files/scripts/download_openhab.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-wget -q -—no-check-certificate --no-cookies -O /tmp/distribution-runtime.zip https://github.com/openhab/openhab/releases/download/v$OPENHAB_VERSION/distribution-$OPENHAB_VERSION-runtime.zip 
-wget —q —-no-check-certificate --no-cookies -O /tmp/distribution-addons.zip https://github.com/openhab/openhab/releases/download/v$OPENHAB_VERSION/distribution-$OPENHAB_VERSION-addons.zip
-wget -q —-no-check-certificate --no-cookies -O /tmp/demo-openhab.zip https://github.com/openhab/openhab/releases/download/v$OPENHAB_VERSION/distribution-$OPENHAB_VERSION-demo-configuration.zip
-wget —q —-no-check-certificate --no-cookies -O /tmp/org.openhab.io.myopenhab-1.4.0-SNAPSHOT.jar https://my.openhab.org/downloads/org.openhab.io.myopenhab-1.4.0-SNAPSHOT.jar
+
+if [[ $OPENHAB_VERSION == "SNAPSHOT" ]]
+then
+  wget --quiet --no-check-certificate --no-cookies -O /tmp/distribution-runtime.zip https://openhab.ci.cloudbees.com/job/openHAB/lastBuild/artifact/distribution/target/distribution-1.7.0-SNAPSHOT-runtime.zip
+  wget --quiet --no-check-certificate --no-cookies -O /tmp/distribution-addons.zip https://openhab.ci.cloudbees.com/job/openHAB/lastBuild/artifact/distribution/target/distribution-1.7.0-SNAPSHOT-addons.zip
+  wget --quiet --no-check-certificate --no-cookies -O /tmp/demo-openhab.zip https://openhab.ci.cloudbees.com/job/openHAB/lastBuild/artifact/distribution/target/distribution-1.7.0-SNAPSHOT-demo.zip
+else
+  wget --quiet --no-check-certificate --no-cookies -O /tmp/distribution-runtime.zip https://github.com/openhab/openhab/releases/download/v$OPENHAB_VERSION/distribution-$OPENHAB_VERSION-runtime.zip
+  wget --quiet --no-check-certificate --no-cookies -O /tmp/distribution-addons.zip https://github.com/openhab/openhab/releases/download/v$OPENHAB_VERSION/distribution-$OPENHAB_VERSION-addons.zip
+  wget --quiet --no-check-certificate --no-cookies -O /tmp/demo-openhab.zip https://github.com/openhab/openhab/releases/download/v$OPENHAB_VERSION/distribution-$OPENHAB_VERSION-demo-configuration.zip
+fi
+
+wget --quiet --no-check-certificate --no-cookies -O /tmp/org.openhab.io.myopenhab-1.4.0-SNAPSHOT.jar https://my.openhab.org/downloads/org.openhab.io.myopenhab-1.4.0-SNAPSHOT.jar
 
 rm -rf /opt/openhab
 mkdir -p /opt/openhab/addons-available
 mkdir -p /opt/openhab/logs
-unzip -d /opt/openhab /tmp/distribution-runtime.zip
-unzip -d /opt/openhab/addons-available /tmp/distribution-addons.zip
-unzip -d /opt/openhab/demo-configuration /tmp/demo-openhab.zip
+unzip -q -d /opt/openhab /tmp/distribution-runtime.zip
+unzip -q -d /opt/openhab/addons-available /tmp/distribution-addons.zip
+unzip -q -d /opt/openhab/demo-configuration /tmp/demo-openhab.zip
 chmod +x /opt/openhab/start.sh
 mv /tmp/org.openhab.io.myopenhab-1.4.0-SNAPSHOT.jar /opt/openhab/addons-available
 mv /opt/openhab/configurations /etc/openhab
